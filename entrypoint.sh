5adf2f1017d6722c775aa10da40556c05f4dc134
Siwat Sirichai <siwat@siwatinc.com>
2022-03-31 23:22:53 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ curl -X PUT --data-binary @/software/unit.json --unix-socket /var/run/control-un
 certbot certonly --test-cert --webroot --agree-tos -m $EMAIL -n -w /ssl/certbotroot -d $DOMAIN_NAME &&
 
 #Construct and Upload Certificate to NGINX Unit
-cat /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem > fullcert.pem
-cat /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem >> fullcert.pem
-curl -X PUT --data-binary @fullcert.pem --unix-socket /var/run/control-unit.sock http://localhost/certificates/primary
+cat /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem > fullcert.pem &&
+cat /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem >> fullcert.pem &&
+curl -X PUT --data-binary @fullcert.pem --unix-socket /var/run/control-unit.sock http://localhost/certificates/primary &&
 
 #Apply SSL Partial Configuration
 curl -X PUT --data-binary @/software/ssl-listener.json --unix-socket /var/run/control-unit.sock 'http://localhost/config/listener/%2A%3A443' &&
