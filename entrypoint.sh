f3ecadf0110a266d8e9b3756ae2562d8edd87cc5
Siwat Sirichai <siwat@siwatinc.com>
2022-03-31 23:48:00 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ cat /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem >> fullcert.pem &&
 curl -X PUT --data-binary @fullcert.pem --unix-socket /var/run/control-unit.sock http://localhost/certificates/primary &&
 
 #Apply SSL Partial Configuration
-curl -X PUT --data-binary @/software/listener.json --unix-socket /var/run/control-unit.sock 'http://localhost/config/listeners/%2A%3A443' &&
+curl -X PUT --data-binary @/software/ssl-listener.json --unix-socket /var/run/control-unit.sock 'http://localhost/config/listeners/%2A%3A443' &&
 
 #Idle Forever (IDK why tail -f /dev/null doesn't work so using infinite loop instead.)
 #If you use other base image other than minimal, tail might work.
