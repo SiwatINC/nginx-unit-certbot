3e4f63c357850c6f29fbfd932cd024328feeaef6
Siwat Sirichai <siwat@siwatinc.com>
2022-03-31 22:37:31 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
+#Start Unit
 unitd --no-daemon --control unix:/var/run/control-unit.sock &
+
+#Apply Primary Full Configuration
 curl -X PUT --data-binary @/software/unit.json --unix-socket /var/run/control-unit.sock http://localhost/config &&
+
+#Request Certificate
 certbot certonly --test-cert --webroot --agree-tos -m $EMAIL -n -w /ssl/certbotroot -d $DOMAIN_NAME &&
 
 #Upload Certificate to NGINX Unit
 curl -X PUT --data-binary @/etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem --unix-socket /var/run/control-unit.sock http://localhost/certificates/primary
 
+#Apply SSL Partial Configuration
 curl -X PUT --data-binary @/software/ssl-config.json --unix-socket /var/run/control-unit.sock http://localhost/config/ssl &&
 
 #Idle Forever
