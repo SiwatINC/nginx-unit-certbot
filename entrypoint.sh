5b4c2fe2e9aefffb9b1d7b60f0758800cce7c354
Siwat Sirichai <siwat@siwatinc.com>
2022-04-06 00:13:06 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,12 @@ curl -X PUT --data-binary @/software/unit.json --unix-socket /var/run/control-un
 (certbot certonly --webroot --agree-tos -m $EMAIL -n -w /ssl/certbotroot -d $DOMAIN_NAME || :) &&
 
 #Construct and Upload Certificate to NGINX Unit
-cat /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem > fullcert.pem &&
-cat /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem >> fullcert.pem &&
-curl -X PUT --data-binary @fullcert.pem --unix-socket /var/run/control-unit.sock http://localhost/certificates/primary &&
+(cat /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem > fullcert.pem || :) &&
+(cat /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem >> fullcert.pem || :) &&
+(curl -X PUT --data-binary @fullcert.pem --unix-socket /var/run/control-unit.sock http://localhost/certificates/primary || :) &&
 
 #Apply SSL Partial Configuration
-curl -X PUT --data-binary @/software/ssl-listener.json --unix-socket /var/run/control-unit.sock 'http://localhost/config/listeners/*:443' &&
+(curl -X PUT --data-binary @/software/ssl-listener.json --unix-socket /var/run/control-unit.sock 'http://localhost/config/listeners/*:443' || :) &&
 
 #Idle Forever
 tail -f /dev/null
